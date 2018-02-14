6033830176cf28a5844a68db426c7a3adde5a2be
Dan Mulvihill <dmulvihill3@gmail.com>
2018-02-13 21:19:05 -0500

    fixed js bug

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 function goodFunc(){
     document.getElementById("polk-pic").src ="img/good-polk.jpg";
+    
     var hero = document.getElementById("hero");
+    var demon = document.getElementById("demon");
     hero.style.color ="blue";
     hero.style.fontWeight = "bold";
+    demon.style.color ="black";
+    demon.style.fontWeight = "normal";  
 
 }
 
 function badFunc(){
     document.getElementById("polk-pic").src ="img/evil-polk.jpg";
     var demon = document.getElementById("demon");
+    var hero = document.getElementById("hero");
     demon.style.color ="red";
     demon.style.fontWeight = "bold";
+    hero.style.color = "black";
+    hero.style.fontWeight = "normal";
 }
