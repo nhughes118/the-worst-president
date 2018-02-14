c12339772c53354677db9c687e595f0eea91d803
Dan Mulvihill <dmulvihill3@gmail.com>
2018-02-14 12:57:34 -0500

    added a setTimout behavior to button to return photo and subheading back to normal after 3 secs

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-function goodFunc(){
+function makeGood(){
     document.getElementById("polk-pic").src ="img/good-polk.jpg";
     
     var hero = document.getElementById("hero");
     var demon = document.getElementById("demon");
     hero.style.color ="blue";
     hero.style.fontWeight = "bold";
-    demon.style.color ="black";
-    demon.style.fontWeight = "normal";  
+    setTimeout(returnToNormal, 3000);
+
 
 }
 
-function badFunc(){
+function makeBad(){
     document.getElementById("polk-pic").src ="img/evil-polk.jpg";
     var demon = document.getElementById("demon");
     var hero = document.getElementById("hero");
     demon.style.color ="red";
     demon.style.fontWeight = "bold";
+    setTimeout(returnToNormal, 3000);
+
+}
+
+function returnToNormal(){
+    document.getElementById("polk-pic").src ="img/James-Polk.jpeg";
     hero.style.color = "black";
     hero.style.fontWeight = "normal";
+    demon.style.color ="black";
+    demon.style.fontWeight = "normal";  
 }
