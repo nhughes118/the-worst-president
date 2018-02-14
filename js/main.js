79d4c6b33bc3769cb8e34bb086ab73ed2540e433
Dan Mulvihill <dmulvihill3@gmail.com>
2018-02-13 21:11:14 -0500

    subtitle color variation added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 function goodFunc(){
     document.getElementById("polk-pic").src ="img/good-polk.jpg";
+    var hero = document.getElementById("hero");
+    hero.style.color ="blue";
+    hero.style.fontWeight = "bold";
+
 }
 
 function badFunc(){
     document.getElementById("polk-pic").src ="img/evil-polk.jpg";
+    var demon = document.getElementById("demon");
+    demon.style.color ="red";
+    demon.style.fontWeight = "bold";
 }
