5b5fd6aeaa43d9ee0b0b790732ab73ad5689ebb8
Dan Mulvihill <dmulvihill3@gmail.com>
2018-02-19 13:12:10 -0500

    cleaned up code and some (most unsuccessful) attempts to add jQuery

--- js/main.js
+++ js/main.js
@@ -1,36 +1,61 @@
 
-var goodPrez = document.getElementsByClassName("good-president");
-for (var i = 0; i < goodPrez.length; i++) {
-    goodPrez[i].addEventListener('click', makeGood);
-}
+$(document).ready(function(){
 
-var badPrez = document.getElementsByClassName("bad-president");
-for (var i = 0; i < badPrez.length; i++) {
-    badPrez[i].addEventListener('click', makeBad);
-}
+    var virtueScore = 0;
 
-function makeGood(){
-    document.getElementById("polk-pic").src ="img/good-polk.jpg";  
-    var hero = document.getElementById("hero");
-    var demon = document.getElementById("demon");
-    hero.style.color ="blue";
-    hero.style.fontWeight = "bold";
-    setTimeout(returnToNormal, 3000);
-}
+    var goodPrez = document.getElementsByClassName("good-president");
+    for (var i = 0; i < goodPrez.length; i++) {
+        goodPrez[i].addEventListener('click', makeGood);
+    }
 
-function makeBad(){
-    document.getElementById("polk-pic").src ="img/evil-polk.jpg";
-    var demon = document.getElementById("demon");
-    var hero = document.getElementById("hero");
-    demon.style.color ="red";
-    demon.style.fontWeight = "bold";
-    setTimeout(returnToNormal, 3000);
-}
+    var badPrez = document.getElementsByClassName("bad-president");
+    for (var i = 0; i < badPrez.length; i++) {
+        badPrez[i].addEventListener('click', makeBad);
+    }
 
-function returnToNormal(){
-    document.getElementById("polk-pic").src ="img/James-Polk.jpeg";
-    hero.style.color = "black";
-    hero.style.fontWeight = "normal";
-    demon.style.color ="black";
-    demon.style.fontWeight = "normal";  
-}
+    function makeGood(){
+        virtueScore += 1;
+        console.log(virtueScore);
+        document.getElementById("polk-pic").src ="img/good-polk.jpg";  
+        var hero = document.getElementById("hero");
+        var demon = document.getElementById("demon");
+        hero.style.color ="blue";
+        hero.style.fontWeight = "bold";
+        setTimeout(returnToNormal, 1000);
+        if (virtueScore >= 3){
+            virtueScore = 0;
+            //$("#js-sub").html("<h2>Hero President<h2>");
+            //$("#polk-pic").slideUp();
+        }
+    }
+
+    function makeBad(){
+        virtueScore -= 1;
+        console.log(virtueScore);
+        document.getElementById("polk-pic").src ="img/evil-polk.jpg";
+        var demon = document.getElementById("demon");
+        var hero = document.getElementById("hero");
+        demon.style.color ="red";
+        demon.style.fontWeight = "bold";
+        setTimeout(returnToNormal, 1000);
+        if (virtueScore <= -3 ){
+            virtueScore = 0;
+            //$("#js-sub").html("<h2>Evil Demon<h2>");
+            //$("#polk-pic").slideDown();
+        }
+    }
+
+    function returnToNormal(){
+        document.getElementById("polk-pic").src ="img/James-Polk.jpeg";
+        if(hero){
+            hero.style.color = "black";
+            hero.style.fontWeight = "normal";
+        }
+        if(demon){
+            demon.style.color ="black";
+            demon.style.fontWeight = "normal";  
+        }
+
+    }
+
+});
